a332a297118045f761e73df37e1ca18b0c4e9289
Ali Alhoshaiyan <alialhoshaiyan@gmail.com>
2021-06-09 16:52:06 +0300

    Add Example ASP.NET Core Apple Pay Payments with Moyasar

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ApplePayDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("/validate-merchant")]
+        public async Task<IActionResult> ValidateMerchant([FromBody] FrontEndValidationRequest request)
+        {
+            // .Net requires a PKCS12 certificate file which combines both key and cert
+            // To combine your certs, use the following (Requires OpenSSL supported):
+            // openssl pkcs12 -export -in merchant_id.pem -inkey merchant_id.key -out cert.pfx
+            var certFile = await System.IO.File.ReadAllBytesAsync("merchant_id/cert.pfx");
+            var cert = new X509Certificate2(certFile, "123");
+
+            var opt = new HttpClientHandler();
+            opt.ClientCertificates.Add(cert);
+            var client = new HttpClient(opt);
+            
+            var requestBody = new ValidateMerchantRequest
+            {
+                merchantIdentifier = "merchant.com.example",
+                displayName = "Coffee Store",
+                initiative = "web",
+                initiativeContext = "example.com"
+            };
+            
+            using (var content = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.Default, "application/json"))
+            using (var response = await client.PostAsync(request.ValidationUrl, content))
+            {
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                }
+                catch
+                {
+                    // TODO: Set Content-Type: application.json
+                    return BadRequest(await response.Content.ReadAsStringAsync());
+                }
+                
+                return Content(await response.Content.ReadAsStringAsync(), "application/json");
+            }
+        }
+    }
+    
+    class ValidateMerchantRequest
+    {
+        public string merchantIdentifier { get; set; }
+        public string displayName { get; set; }
+        public string initiative { get; set; }
+        public string initiativeContext { get; set; }
+    }
+
+    public class FrontEndValidationRequest
+    {
+        public string ValidationUrl { get; set; }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,110 @@
+﻿window.applePaySession = null;
+let applePayButton = document.querySelector('.apple-pay-button');
+
+if (! (window.ApplePaySession && window.ApplePaySession.canMakePayments())) {
+    applePayButton.remove();
+    
+    let h4 = document.createElement('h4');
+    h4.innerText = 'Apple Pay is not supported, please use Safari on a supported macOS version.';
+    document.body.appendChild(h4);
+}
+
+if (applePayButton) {
+    applePayButton.addEventListener('click', initApplePay);
+}
+
+function initApplePay() {
+    let request = {
+        countryCode: 'SA',
+        currencyCode: 'SAR',
+        supportedNetworks: ['visa', 'masterCard', 'amex', 'mada'],
+        merchantCapabilities: ['supports3DS'],
+        total: {
+            label: 'Coffee Store',
+            amount: '1.00'
+        }
+    };
+
+    window.applePaySession = new ApplePaySession(6, request);
+    window.applePaySession.onvalidatemerchant = onMerchantValidation;
+    window.applePaySession.onpaymentauthorized = onPaymentAuthorized;
+    window.applePaySession.oncancel = onApplePayCanceled;
+    
+    window.applePaySession.begin();
+}
+
+function onMerchantValidation(event) {
+    fetch('/validate-merchant', {
+        method: 'post',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'validationUrl': event.validationURL
+        })
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            window.applePaySession.completeMerchantValidation(res)
+        })
+        .catch(error => console.log(`Could not validate merchant ${error}`));
+}
+
+function onPaymentAuthorized(event) {
+    const paymentData = event.payment.token.paymentData;
+    
+    // Complete Payment with Moyasar
+    fetch('https://api.moyasar.com/v1/payments', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'publishable_api_key': 'put_your_api_key_here',
+            'amount': '100',
+            'currency': 'SAR',
+            'description': 'Apple Pay Payment Demo in ASP.NET Core',
+            'source': {
+                'type': 'applepay',
+                'token': JSON.stringify(paymentData)
+            }
+        })
+    })
+        .then(res => res.ok ? onMoyasarResponse(res) : onMoyasarError(res))
+        .catch(error => console.log(`Could not create payment at Moyasar: ${error}`));
+}
+
+function onMoyasarResponse(res) {
+    res.json()
+        .then(payment => {
+            if (payment.status === 'paid') {
+                window.applePaySession.completePayment({
+                    'status': ApplePaySession.STATUS_SUCCESS
+                });
+            } else {
+                window.applePaySession.completePayment({
+                    'status': ApplePaySession.STATUS_FAILURE,
+                    'errors': [
+                        payment.source.message
+                    ]
+                });
+            }
+        })
+        .catch(error => console.log(error));
+}
+
+function onMoyasarError(res) {
+    res.json()
+        .then(payment => {
+            console.log(payment.message);
+        })
+        .catch(error => console.log(error));
+}
+
+function onApplePayCanceled(event) {
+    console.log('Apple Pay was canceled');
+    console.log(event);
+    window.applePaySession = null;
+}
